d2f3768456b29c19f048be5efa9440ad878518b4
Mark <markmccoid@gmail.com>
2024-02-13 20:14:29 -0500

    initial commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,14 +1,33 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { Link, useRouter } from "expo-router";
 
 export default function TabOneScreen() {
+  const router = useRouter();
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/index.tsx" />
+
+      {/* Link Component */}
+      <Link href={{ pathname: "/(tabs)/two", params: { id: "/123#test#" } }}>
+        Link Without Parens
+      </Link>
+      <Link href={{ pathname: "/(tabs)/two", params: { id: "/123(test)" } }}>Link WITH Parens</Link>
+      <View style={{ height: 2, backgroundColor: "red", width: "100%", marginVertical: 10 }} />
+      {/* router.push() */}
+      <TouchableOpacity
+        onPress={() => router.push({ pathname: "/(tabs)/two", params: { id: "/123#test#" } })}
+      >
+        <Text>Push Without Parens</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => router.push({ pathname: "/(tabs)/two", params: { id: "/123(test)" } })}
+      >
+        <Text>Push WITH Parens</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,16 +35,16 @@ export default function TabOneScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
--- app/(tabs)/two.tsx
+++ app/(tabs)/two.tsx
@@ -1,14 +1,20 @@
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from '@/components/EditScreenInfo';
-import { Text, View } from '@/components/Themed';
+import EditScreenInfo from "@/components/EditScreenInfo";
+import { Text, View } from "@/components/Themed";
+import { useGlobalSearchParams, useLocalSearchParams } from "expo-router";
 
 export default function TabTwoScreen() {
+  const { id } = useLocalSearchParams();
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Tab Two</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/two.tsx" />
+      <View style={{ flexDirection: "row", alignItems: "center" }}>
+        <Text>Passed Param: </Text>
+        <Text>{id || "Params NOT RECEIVED"}</Text>
+      </View>
     </View>
   );
 }
@@ -16,16 +22,16 @@ export default function TabTwoScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
   title: {
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   separator: {
     marginVertical: 30,
     height: 1,
-    width: '80%',
+    width: "80%",
   },
 });
